328cadee551750e8ed9c46ce9e33d720742aa567
Hodong2 <Hodong2@DESKTOP-PQBOHJ3>
2023-06-15 22:23:31 +0900

    test

--- MiniMW/src/main/java/com/jhd/hops/minimw/security/jwt/filter/JwtAuthenticationFilter.java
+++ MiniMW/src/main/java/com/jhd/hops/minimw/security/jwt/filter/JwtAuthenticationFilter.java
@@ -30,7 +30,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-			throws ServletException, IOException {
+			throws ServletException, IOException { 
 		String token = "";
 		try {
 			token = getToken(request);
